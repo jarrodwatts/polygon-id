621fe1ce4ff1304ee2c66e860b03526afc925e8c
Jarrod Watts <jarrodwatts16@gmail.com>
2023-07-06 09:56:09 +1000

    app metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,30 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 import "@/styles/globals.css";
+import Head from "next/head";
 
 const queryClient = new QueryClient();
 
 /**
- * Wrap the application in TanStack Query's QueryClientProvider
+ * Standard Next.js App component that wraps the entire application.
+ * It uses the react-query QueryClientProvider to provide the query client
+ * to all pages and components.
  */
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
-    </QueryClientProvider>
+    <>
+      <Head>
+        <title>Polygon ID Demo</title>
+        <meta
+          name="description"
+          content="A starter kit for building full-stack applications with Polygon ID."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </>
   );
 }
